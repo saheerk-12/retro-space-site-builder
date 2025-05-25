1d1b15c4d95c543eb405bed8fef98837078724dd
Saheer Kottakkadan <saheer.kottakkadan@givenergy.co.uk>
2025-05-25 17:42:55 +0530

    Fix client-side routing for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -14,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/retro-space-site-builder">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/about" element={<NotFound />} />
